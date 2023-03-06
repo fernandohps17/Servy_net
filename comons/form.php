834c8d67a5c0ed6f067c969a1d2b75a8ec8b1fea
fernandohps17 <codetechweb1@gmail.com>
2023-03-06 00:02:20 -0400

    cambios en el boton de ws(numero) y favicon

--- comons/form.php
+++ comons/form.php
@@ -1,71 +1,74 @@
 <div class="container bg_form">
-    <div class="content">
-      <div class="form">
-        <div class="form_contact">
-          <div class="form_contact_content">
-            <div class="form_contact_phone">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"> <path d="M39.75 42q-6.1 0-12.125-3T16.8 31.2Q12 26.4 9 20.375 6 14.35 6 8.25q0-.95.65-1.6Q7.3 6 8.25 6h7q.7 0 1.225.475.525.475.675 1.275l1.35 6.3q.1.7-.025 1.275t-.525.975l-5 5.05q2.8 4.65 6.275 8.1Q22.7 32.9 27.1 35.3l4.75-4.9q.5-.55 1.15-.775.65-.225 1.3-.075l5.95 1.3q.75.15 1.25.75T42 33v6.75q0 .95-.65 1.6-.65.65-1.6.65Zm-28.3-23.4 4.05-4.1L14.35 9H9q0 1.95.6 4.275t1.85 5.325ZM29.9 36.75q2.05.95 4.45 1.55 2.4.6 4.65.7v-5.35l-5.15-1.05ZM11.45 18.6ZM29.9 36.75Z" /></svg>
-              <a href="tel:34856500700" onclick="gtag('event', 'telefonos')"><b>856 500 700</b></a> <a href="tel:34681215521" onclick="gtag('event', 'telefonos')"><b>681 21 55 21</b></a>
-            </div>
-            <div class="form_content">
-              <div class="form_content_ws">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" /></svg>
-                <a href="https://api.whatsapp.com/send?phone=34627961291" target="_blank"
-                  onclick="gtag('event', 'whatsapp')"><strong>Enviar Whatsapp</strong></a>
-              </div>
-            </div>
+  <div class="content">
+    <div class="form">
+      <div class="form_contact">
+        <div class="form_contact_content">
+          <div class="form_contact_phone">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+              <path d="M39.75 42q-6.1 0-12.125-3T16.8 31.2Q12 26.4 9 20.375 6 14.35 6 8.25q0-.95.65-1.6Q7.3 6 8.25 6h7q.7 0 1.225.475.525.475.675 1.275l1.35 6.3q.1.7-.025 1.275t-.525.975l-5 5.05q2.8 4.65 6.275 8.1Q22.7 32.9 27.1 35.3l4.75-4.9q.5-.55 1.15-.775.65-.225 1.3-.075l5.95 1.3q.75.15 1.25.75T42 33v6.75q0 .95-.65 1.6-.65.65-1.6.65Zm-28.3-23.4 4.05-4.1L14.35 9H9q0 1.95.6 4.275t1.85 5.325ZM29.9 36.75q2.05.95 4.45 1.55 2.4.6 4.65.7v-5.35l-5.15-1.05ZM11.45 18.6ZM29.9 36.75Z" />
+            </svg>
+            <a href="tel:34856500700" onclick="gtag('event', 'telefonos')"><b>856 500 700</b></a> <a href="tel:34681215521" onclick="gtag('event', 'telefonos')"><b>681 21 55 21</b></a>
           </div>
-          <div class="form_title">
-            <h2><b> Analizamos tu web </b><br> GRATIS y sin compromiso</h2>
-          </div>
-          <div class="form_subtitle">
-            <p>Rellena el formulario y uno de nuestros técnicos se pondrá en contacto contigo para realizar una
-              auditoría completa de tu página web.</p>
+          <div class="form_content">
+            <div class="form_content_ws">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" />
+              </svg>
+              <a href="https://api.whatsapp.com/send?phone=34681215521" target="_blank" onclick="gtag('event', 'whatsapp')"><strong>Enviar Whatsapp</strong></a>
+            </div>
           </div>
         </div>
-        <div class="form_form">
-          <form class="section_form_inputs" action="./contact/" method="POST">
-            <div class="section_from_uno">
-              <label class="section_form_inputs_encabezado">
-                Nombre y Apellido
-                <input type="text" name="name" required>
-              </label>
-
-              <label class="section_form_inputs_encabezado">
-                Teléfono
-                <input type="text" name="phone" required>
-              </label>
-            </div>
-            <div class="section_from_uno">
-              <label class="section_form_inputs_encabezado">
-                E-mail
-                <input type="text" name="email" required>
-              </label>
-
-              <label class="section_form_inputs_encabezado">
-                URL web a analizar
-                <input type="text" name="name" required>
-              </label>
-            </div>
-
+        <div class="form_title">
+          <h2><b> Analizamos tu web </b><br> GRATIS y sin compromiso</h2>
+        </div>
+        <div class="form_subtitle">
+          <p>Rellena el formulario y uno de nuestros técnicos se pondrá en contacto contigo para realizar una
+            auditoría completa de tu página web.</p>
+        </div>
+      </div>
+      <div class="form_form">
+        <form class="section_form_inputs" action="./contact/" method="POST">
+          <div class="section_from_uno">
             <label class="section_form_inputs_encabezado">
-              Notas
-              <input type="text" name="notas" required>
+              Nombre y Apellido
+              <input type="text" name="name" required>
             </label>
 
-            <div class="section_form_inputs_botones">
-              <label class="section_form_terminos">
-                <input class="checkbox" type="checkbox" required>
-                <span>He leído y aceptado la <a href="/politicas-de-privacidad/"><u>política de privacidad</u></a></span>
-              </label>
+            <label class="section_form_inputs_encabezado">
+              Teléfono
+              <input type="text" name="phone" required>
+            </label>
+          </div>
+          <div class="section_from_uno">
+            <label class="section_form_inputs_encabezado">
+              E-mail
+              <input type="text" name="email" required>
+            </label>
 
-              <div class="section_form_boton">
-                <input type="hidden" class="g-recaptcha-response" name="g-recaptcha-response">
-                <button type="submit">Enviar</button>
-              </div>
+            <label class="section_form_inputs_encabezado">
+              URL web a analizar
+              <input type="text" name="name" required>
+            </label>
+          </div>
+
+          <label class="section_form_inputs_encabezado">
+            Notas
+            <input type="text" name="notas" required>
+          </label>
+
+          <div class="section_form_inputs_botones">
+            <label class="section_form_terminos">
+              <input class="checkbox" type="checkbox" required>
+              <span>He leído y aceptado la <a href="/politicas-de-privacidad/"><u>política de privacidad</u></a></span>
+            </label>
+
+            <div class="section_form_boton">
+              <input type="hidden" class="g-recaptcha-response" name="g-recaptcha-response">
+              <button type="submit">Enviar</button>
             </div>
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
     </div>
   </div>
+</div>
