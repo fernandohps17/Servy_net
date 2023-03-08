73c39ab07bde12c4bde64bc9d7f1c495ea17bdb4
fernandohps17 <codetechweb1@gmail.com>
2023-03-08 01:07:57 -0400

    Cambios en footer y responsive

--- comons/footer.php
+++ comons/footer.php
@@ -1,7 +1,7 @@
   <div class="container bg_footer_top">
     <div class="content">
       <div class="footer_top">
-        <div class="footer_uno">
+        <div class="footer_uno modificado">
           <div class="footer_icons">
             <div class="footer_svg">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960"><path d="M795 936q-122 0-242.5-60T336 720q-96-96-156-216.5T120 261q0-19.286 12.857-32.143T165 216h140q13.611 0 24.306 9.5Q340 235 343 251l27 126q2 14-.5 25.5T359 422L259 523q56 93 125.5 162T542 802l95-98q10-11 23-15.5t26-1.5l119 26q15.312 3.375 25.156 15.188Q840 740 840 756v135q0 19.286-12.857 32.143T795 936ZM229 468l81-82-23-110H180q0 39 12 85.5T229 468Zm369 363q41 19 89 31t93 14V769l-103-21-79 83ZM229 468Zm369 363Z"/></svg>
@@ -12,7 +12,16 @@
           <div class="footer_icons">
             <div class="footer_svg">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960"><path d="m627 769 45-45-159-160V363h-60v225l174 181ZM480 976q-82 0-155-31.5t-127.5-86Q143 804 111.5 731T80 576q0-82 31.5-155t86-127.5Q252 239 325 207.5T480 176q82 0 155 31.5t127.5 86Q817 348 848.5 421T880 576q0 82-31.5 155t-86 127.5Q708 913 635 944.5T480 976Zm0-400Zm0 340q140 0 240-100t100-240q0-140-100-240T480 236q-140 0-240 100T140 576q0 140 100 240t240 100Z"/></svg>
-              <p><strong>HORARIO:</strong> De 9:00h a 14:00h. y de 17:00h a 20:00h. (De Lunes a Viernes) <br> <strong>URGENCIAS:</strong> 24H. (Para reparar WEBS caidas, sin funcionar)</p>
+              <p><strong>HORARIO:</strong> De 9:00h a 14:00h. y de 17:00h a 20:00h. (De Lunes a Viernes) 
+            </div>
+          </div>
+
+          <div class="footer_icons">
+            <div class="footer_svg">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960"><path d="m627 769 45-45-159-160V363h-60v225l174 181ZM480 976q-82 0-155-31.5t-127.5-86Q143 804 111.5 731T80 576q0-82 31.5-155t86-127.5Q252 239 325 207.5T480 176q82 0 155 31.5t127.5 86Q817 348 848.5 421T880 576q0 82-31.5 155t-86 127.5Q708 913 635 944.5T480 976Zm0-400Zm0 340q140 0 240-100t100-240q0-140-100-240T480 236q-140 0-240 100T140 576q0 140 100 240t240 100Z"/></svg>
+              <p>
+                <strong>URGENCIAS:</strong> 24H. (Para reparar WEBS caidas, sin funcionar)</p>
+              </p>
             </div>
           </div>
 
